caa8b98b6668a8714993d48a7ebc7147a714e982
hackett91 <C16723031@mydit.ie>
2018-03-02 04:32:26 +0000

    testing ssh

--- java/udemy_oop/exercise1/Zoo.java
+++ java/udemy_oop/exercise1/Zoo.java
@@ -15,7 +15,7 @@ public class Zoo{
 		crow = new Bird(1, 2, "male");
 		// because a bird inherits from it parent class
 		// animal it can use the eat and sleep funcitonality
-	//yesterday
+
 		crow.eat();
 		crow.sleep();
 		Chicken chick;
